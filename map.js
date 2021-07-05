ad469f8b0a4d1aca11ed353fd71f7c91e93a8ebf
Your Name <liuxun1230@hotmail.com>
2021-07-05 00:18:09 -0400

    live data
    live data link to google sheet

--- map.js
+++ map.js
@@ -3,6 +3,22 @@ var mapCenter = [0,30];
 var mapZoom = 1.5;
 
 
+//-----------------------------------------------------------
+//Live Data link to google sheet Part A
+
+
+var transformRequest = (url, resourceType) => {
+  var isMapboxRequest =
+    url.slice(8, 22) === "api.mapbox.com" ||
+    url.slice(10, 26) === "tiles.mapbox.com";
+  return {
+    url: isMapboxRequest
+      ? url.replace("?", "?pluginName=sheetMapper&")
+      : url
+  };
+};
+
+
 // --------------------------------------------------------
 // 1. Initialize map
     mapboxgl.accessToken = 'pk.eyJ1IjoieHVubGl1IiwiYSI6ImNrMXV5bzV2ZzA0aGUzaG16YWJybHllbjAifQ.11sHwcv-F_pZQpmW5PZxNw'; // replace this value with your own access token from Mapbox Studio
@@ -12,8 +28,87 @@ var mapZoom = 1.5;
     	container: 'map', // this is the ID of the div in index.html where the map should go
         center: mapCenter, // set the centerpoint of the map programatically. Note that this is [longitude, latitude]!
         zoom: mapZoom, // set the default zoom programatically
-    	style: 'mapbox://styles/xunliu/ckqhk5f702reb17p7iakske5x'// replace this value with the style URL from Mapbox Studio
+    	style: 'mapbox://styles/xunliu/ckqhk5f702reb17p7iakske5x',// replace this value with the style URL from Mapbox Studio
+        transformRequest: transformRequest
     });
+
+//-----------------------------------------------------------
+//Live Data link to google sheet Part B
+$(document).ready(function () {
+      $.ajax({
+        type: "GET",
+        //YOUR TURN: Replace with csv export link
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRSyFzraNTlQbU2TNkrIp6Jc15SgeQKJZigPCA8fVglyhCi3AFhAsGhkaj6c6L6h-gsood5Kbw4NhZb/pub?gid=0&single=true&output=csv',
+        dataType: "text",
+        success: function (csvData) { makeGeoJSON(csvData); }
+      });
+    
+
+      function makeGeoJSON(csvData) {
+        csv2geojson.csv2geojson(csvData, {
+          latfield: 'Latitude',
+          lonfield: 'Longitude',
+          delimiter: ','
+        }, function (err, data) {
+          map.on('load', function () {
+
+            //Add the the layer to the map
+            map.addLayer({
+              'id': 'casesLive',
+              'type': 'symbol',
+              'source': {
+                'type': 'geojson',
+                'data': data
+              },
+              'layout': {
+                'icon-image': 'hospital-15',
+              }
+            });
+
+
+        // Create a popup on click 
+            map.on('click', function(e) {   // Event listener to do some code when user clicks on the map
+
+                // Change the cursor to a pointer when the mouse is over the places layer.
+                map.on('mouseenter',  function () {
+                map.getCanvas().style.cursor = 'pointer';
+                });
+
+                // Change it back to a pointer when it leaves.
+                map.on('mouseleave',  function () {
+                map.getCanvas().style.cursor = '';
+                });
+                var cases = map.queryRenderedFeatures(e.point, { 
+                layers: ['casesLive']    // replace this with the name of the layer from the Mapbox Studio layers panel
+            });
+
+              if (cases.length == 0) {
+                return;
+            }
+
+            var popup = new mapboxgl.Popup({ 
+                closeButton: true, // If true, a close button will appear in the top right corner of the popup. Default = true
+                closeOnClick: true, // If true, the popup will automatically close if the user clicks anywhere on the map. Default = true
+                anchor: 'bottom', 
+                offset: [0,0]
+            });
+
+              popup.setLngLat(cases[0].geometry.coordinates);
+
+              // Set the contents of the popup window
+              popup.setHTML("<h2>" + cases[0].properties.Project +"</h2>"+"<img src='"+cases[0].properties.url+"' width='380'"+"'>"+"<p>Designer: " +cases[0].properties.Designer + "<br>Date: " + cases[0].properties.Date + "<br>Location: " + cases[0].properties.Location + "<br>Description: "+ cases[0].properties.Description + "<br><br><a href ='" + cases[0].properties.Source+ "'>Source</a></p>");
+
+              popup.addTo(map);
+          });  
+
+
+          });
+
+        });
+      };
+    });
+
+
 
 
 // --------------------------------------------------------
@@ -30,45 +125,9 @@ var mapZoom = 1.5;
         $(".modal").fadeToggle();
     });
 
-// --------------------------------------------------------
-//// 3. Creating a legend
-//// See example tutorial at https://docs.mapbox.com/help/tutorials/choropleth-studio-gl-pt-2/#create-arrays-of-intervals-and-colors
-//
-//    var layers = [ // an array of the possible values you want to show in your legend
-//        'Civic Spaces', // Civic Spaces.png
-//        'Community Park', // Community Park.png
-//        'Neighborhood Park', // Neighborhood Park.png
-//        'Cemetery',
-//        'Urban Park',
-//        'Regional Park'
-//    ];
-//
-//    var colors = [ // an array of the color values for each legend item
-//        '#800000',
-//        '#800030',
-//        '#800060',
-//        '#80006c',
-//        '#800090',
-//        '#80009c'
-//    ];
-//
-//    // for loop to create individual legend items
-//    for (i=0; i<layers.length; i++) {
-//        var layer =layers[i]; // name of the current legend item, from the layers array
-//        var color =colors[i]; // color value of the current legend item, from the colors array 
-//        
-//        var itemHTML = "<div><span class='legend-key'></span><span>" + layer + "</span></div>"; // create the HTML for the legend element to be added
-//
-//        var item = $(itemHTML).appendTo("#legend"); // add the legend item to the legend
-//        var legendKey = $(item).find(".legend-key"); // find the legend key (colored circle) for the current item
-//        legendKey.css("background", color); // change the background color of the legend key
-//    }
-
-
-
 
 // -------------------------------------------------------- 
-// 5. Popups
+// Popups
 // See tutorial at https://docs.mapbox.com/help/tutorials/add-points-pt-3/
 // See example of popups on click at https://docs.mapbox.com/mapbox-gl-js/example/popup-on-click/ 
 // See example of popups on hover at https://docs.mapbox.com/mapbox-gl-js/example/popup-on-hover/
@@ -90,7 +149,7 @@ var mapZoom = 1.5;
     var popup = new mapboxgl.Popup({ 
         closeButton: true, // If true, a close button will appear in the top right corner of the popup. Default = true
         closeOnClick: true, // If true, the popup will automatically close if the user clicks anywhere on the map. Default = true
-        anchor: 'bottom', // The popup's location relative to the feature. Options are 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left' and 'bottom-right'. If not set, the popup's location will be set dynamically to make sure it is always visible in the map container.
+        anchor: 'top', // The popup's location relative to the feature. Options are 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left' and 'bottom-right'. If not set, the popup's location will be set dynamically to make sure it is always visible in the map container.
         offset: [0, -15] // A pixel offset from the centerpoint of the feature. Can be a single number, an [x,y] coordinate, or an object of [x,y] coordinates specifying an offset for each of the different anchor options (e.g. 'top' and 'bottom'). Negative numbers indicate left and up.
     });
 
@@ -125,47 +184,6 @@ var mapZoom = 1.5;
   });
 
 
-// -------------------------------------------------------- 
-// Popups
-// See tutorial at https://docs.mapbox.com/help/tutorials/add-points-pt-3/
-// See example of popups on click at https://docs.mapbox.com/mapbox-gl-js/example/popup-on-click/ 
-// See example of popups on hover at https://docs.mapbox.com/mapbox-gl-js/example/popup-on-hover/
-
-    // Create a popup on click 
-    map.on('click', function(e) {   // Event listener to do some code when user clicks on the map
-
-        // Change the cursor to a pointer when the mouse is over the places layer.
-        map.on('mouseenter',  function () {
-        map.getCanvas().style.cursor = 'pointer';
-        });
-
-        // Change it back to a pointer when it leaves.
-        map.on('mouseleave',  function () {
-        map.getCanvas().style.cursor = '';
-        });
-        var cases = map.queryRenderedFeatures(e.point, { 
-        layers: ['Cases']    // replace this with the name of the layer from the Mapbox Studio layers panel
-    });
-
-      if (cases.length == 0) {
-        return;
-    }
-
-    var popup = new mapboxgl.Popup({ 
-        closeButton: true, // If true, a close button will appear in the top right corner of the popup. Default = true
-        closeOnClick: true, // If true, the popup will automatically close if the user clicks anywhere on the map. Default = true
-        anchor: 'top', 
-        offset: [0,0]
-    });
-
-      popup.setLngLat(cases[0].geometry.coordinates);
-
-      // Set the contents of the popup window
-      popup.setHTML("<h2>" + cases[0].properties.Project +"</h2>"+"<img src='"+cases[0].properties.url+"' width='400'"+"'>"+"<p>Designer: " +cases[0].properties.Designer + "<br>Date: " + cases[0].properties.Date + "<br>Location: " + cases[0].properties.Location + "<br>Description: "+ cases[0].properties.Description + "<br><a href ='" + cases[0].properties.Source+ "'>Source</a></p>");
-        
-      popup.addTo(map);
-  });
-
 
 // -------------------------------------------------------- 
 // 6. Show/hide layers
@@ -176,7 +194,7 @@ var mapZoom = 1.5;
         // [layerMachineName, layerDisplayName]
         // layerMachineName is the layer name as written in your Mapbox Studio map layers panel
         // layerDisplayName is the way you want the layer's name to appear in the layers control on the website
-        ['Cases', 'Selected Case Study'],                      // layers[0]
+        ['casesLive', 'Selected Case Study'],                      // layers[0]
         ['Students', 'Students and Teaching Team'],                              // layers[1][1] = 'Parks'
         ['Terrain', 'Terrain'],     
         ['Contour', 'Contour'],
@@ -234,6 +252,8 @@ var mapZoom = 1.5;
         }                   
 
     });
+
+
 
 
 
